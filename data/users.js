b1dcf2c74043fce918907acc021c4548e4b70e30
Carson Keller <carsonjkeller@gmail.com>
2020-09-29 18:05:22 -0600

    Created functions to retrieve user list and access and modify profile friend requests

--- data/users.js
+++ data/users.js
@@ -8,6 +8,29 @@ const colName = 'users';
 
 const settings = { useUnifiedTopology: true };
 
+const getUsers = () => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, function(err, client) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Connected to server to retrieve user list.");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.find({}).toArray(function(err, result) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                        client.close();
+                    };
+                });
+            };
+        });
+    });
+    return iou;
+};
+
 const getUser = (key, value) => {
     const iou = new Promise((resolve, reject) => {
         MongoClient.connect(url, settings, function(err, client) {
@@ -54,7 +77,34 @@ const registerUser = (user) => {
     return iou;
 };
 
+const sendFriendRequest = (userID, friendRequest) => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, function(err, client) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Connected to server to send friend request.");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.updateOne({ _id: ObjectID(userID) },
+                { $push: { friendRequests: { $each: friendRequest } } },
+                function(err, result) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                        client.close();
+                    };
+                });
+            };
+        });
+    });
+    return iou;
+};
+
 module.exports = {
+    getUsers,
     getUser,
     registerUser,
+    sendFriendRequest,
 };
