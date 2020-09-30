875e7fecc1fa6951645dd1588d10e83345f19fc5
Carson Keller <carsonjkeller@gmail.com>
2020-09-30 13:03:31 -0600

    Created function to delete friends

--- data/users.js
+++ data/users.js
@@ -1,3 +1,5 @@
+const { connect } = require('mongodb');
+
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
@@ -151,6 +153,30 @@ const rejectFriendRequest = (userID, friendRequest) => {
     return iou;
 };
 
+const deleteFriend = (userID, friend) => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, function(err, client) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Connected to server to delete friend.");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.updateOne({ _id: ObjectID(userID) },
+                { $pullAll: { friends: friend } }, function(err, result) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                        client.close();
+                    };
+                });
+            };
+        });
+    });
+    return iou;
+};
+
 module.exports = {
     getUsers,
     getUser,
@@ -158,4 +184,5 @@ module.exports = {
     sendFriendRequest,
     acceptFriendRequest,
     rejectFriendRequest,
+    deleteFriend,
 };
