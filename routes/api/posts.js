2bfdbc38dec26ca88d52b07b55f8bfb3198602f0
Carson Keller <carsonjkeller@gmail.com>
2020-10-02 09:21:20 -0600

    Created routers to utilize create and delete posts functions.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,11 +2,33 @@ const express = require('express');
 const Router = express.Router();
 const {
     getPosts,
+    createPost,
+    deletePost,
 } = require('../../data/posts');
 
 Router.get('/', async function(req, res) {
     try {
         const data = await getPosts();
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal server issues, check logs.");
+    };
+});
+
+Router.post('/create', async function(req, res) {
+    try {
+        const data = await createPost(req.body);
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal server issues, check logs.");
+    };
+});
+
+Router.delete('/delete/:id', async function(req, res) {
+    try {
+        const data = await deletePost(req.params.id);
         res.send(data);
     } catch (err) {
         console.log(err);
