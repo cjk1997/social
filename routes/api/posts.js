0bf72375993b2cf2b03d2043534eb163dd0dfcc4
Carson Keller <carsonjkeller@gmail.com>
2020-10-02 09:38:14 -0600

    Created router to utilize editPost function.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,6 +3,7 @@ const Router = express.Router();
 const {
     getPosts,
     createPost,
+    editPost,
     deletePost,
 } = require('../../data/posts');
 
@@ -19,6 +20,16 @@ Router.get('/', async function(req, res) {
 Router.post('/create', async function(req, res) {
     try {
         const data = await createPost(req.body);
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal server issues, check logs.");
+    };
+});
+
+Router.patch('/edit/:id', async function(req, res) {
+    try {
+        const data = await editPost(req.params.id, req.body);
         res.send(data);
     } catch (err) {
         console.log(err);
