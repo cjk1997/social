37c3153201ddd3c17f39ceeebaa34f4e0fa8148e
Carson Keller <carsonjkeller@gmail.com>
2020-10-02 09:20:39 -0600

    Created functions to create and delete posts.

--- data/posts.js
+++ data/posts.js
@@ -1,3 +1,5 @@
+const { set } = require('../app');
+
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
 
@@ -31,6 +33,54 @@ const getPosts = () => {
     return iou;
 };
 
+const createPost = (post) => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, function(err, client) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Connected to server to create post.");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.insertOne(post, function(err, result) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                        client.close();
+                    };
+                });
+            };
+        });
+    });
+    return iou;
+};
+
+const deletePost = (postID) => {
+    const iou = new Promise((resolve, reject) => {
+        MongoClient.connect(url, settings, function(err, client) {
+            if (err) {
+                reject(err);
+            } else {
+                console.log("Connected to server to delete post.");
+                const db = client.db(dbName);
+                const collection = db.collection(colName);
+                collection.deleteOne({ _id: ObjectID(postID) }, function(err, result) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(result);
+                        client.close();
+                    };
+                });
+            };
+        });
+    });
+    return iou;
+};
+
 module.exports = {
     getPosts,
+    createPost,
+    deletePost,
 };
