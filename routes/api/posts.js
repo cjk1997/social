bc20df3103fe63c0a47bf284305720bcb59f0fb5
Carson Keller <carsonjkeller@gmail.com>
2020-10-03 19:10:30 -0600

    Created router to handle comment edit requests.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -8,6 +8,7 @@ const {
     likePost,
     unlikePost,
     postComment,
+    editComment,
     likeComment,
     unlikeComment,
     deletePost,
@@ -68,6 +69,19 @@ Router.patch('/comment/:id', async function(req, res) {
         const body = req.body;
         body[0]._id = ObjectID();
         const data = await postComment(req.params.id, body);
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Internal server issues, check logs.");
+    };
+});
+
+Router.patch('/comment/edit/:id', async function(req, res) {
+    try {
+        const body = req.body;
+        const commentID = body.commentID;
+        const comment = body.comment;
+        const data = await editComment(req.params.id, commentID, comment);
         res.send(data);
     } catch (err) {
         console.log(err);
